063aae83976c72017b2ae7e93e2f4e21d7bf78c0
GabrieleNarzisi <Gabriele.narzisi@gmail.com>
2026-03-13 10:42:04 +0100

    check.cpp

--- check.cpp
+++ check.cpp
@@ -1,43 +1,18 @@
 int check (int i, int j, int tab[32][32]) {
     int c = 0;
-    for (int i = 0; i < 32; i++) {
-        for (int j = 0; j < 32; j++) {
-            if (tab[i- 1][j+ 1] == 1) {
-                c++;
-            }
-            if (tab[i][j+ 1] == 1) {
-                c++;
-            }
-            if (tab[i+ 1][j+ 1] == 1) {
-                c++;
-            }
-            if (tab[i- 1][j] == 1) {
-                c++;
-            }
-            if (tab[i+ 1][j] == 1) {
-                c++;
-            }
-            if (tab[i- 1][j- 1] == 1) {
-                c++;
-            }
-            if (tab[i][j- 1] == 1) {
-                c++;
-            }
-            if (tab[i+ 1][j- 1] == 1) {
-                c++;
-            }
-            if (tab[i][j] == 0) {
-                if (c == 0) {
-                    tab[i][j] = 1;
-                }
-            } else {
-                if (c < 2) {
-                    tab[i][j] = 0;
-                } else if (c > 3) {
-                    tab[i][j] = 0;
-                }
-            }
-            return tab[i][j];
-        }
+    if (i > 0 && j < 31 && tab[i-1][j+1] == 1) c++;
+    if (j < 31 && tab[i][j+1] == 1) c++;
+    if (i < 31 && j < 31 && tab[i+1][j+1] == 1) c++;
+    if (i > 0 && tab[i-1][j] == 1) c++;
+    if (i < 31 && tab[i+1][j] == 1) c++;
+    if (i > 0 && j > 0 && tab[i-1][j-1] == 1) c++;
+    if (j > 0 && tab[i][j-1] == 1) c++;
+    if (i < 31 && j > 0 && tab[i+1][j-1] == 1) c++;
+    if (tab[i][j] == 0) {
+        if (c == 3) return 1; 
+    } else {
+        if (c < 2 || c > 3) return 0; 
     }
+    
+    return tab[i][j]; 
 }
