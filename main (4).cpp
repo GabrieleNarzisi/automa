8574c6a5c95670014eb7bdd1604a68713f5ae81b
GabrieleNarzisi <Gabriele.narzisi@gmail.com>
2026-03-13 10:42:37 +0100

    main (4).cpp

--- main (4).cpp
+++ main (4).cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
     int tab[32][32];
     int x;
+    int y;
     srand(time(0));
     for (int i = 0; i < 32; i++) {
         for (int j = 0; j < 32; j++) {
@@ -18,18 +19,27 @@ int main() {
         }
     }
 
-    for (int i = 0; i < 32; i++) {
-        for (int j = 0; j < 32; j++) {
-            check(i, j, tab);
+    while (1 == 1) {
+        y = 0;
+        int tab2[32][32];
+        for (int i = 0; i < 32; i++) {
+            for (int j = 0; j < 32; j++) {
+                tab2[i][j] = check(i, j, tab);
+            }
         }
-    }
-    for (int i = 0; i < 32; i++) {
-        for (int j = 0; j < 32; j++) {
-            cout << tab[i][j] << " ";
+        for (int i = 0; i < 32; i++) {
+            for (int j = 0; j < 32; j++) {
+                tab[i][j] = tab2[i][j];
+                if (tab[i][j] == 1) {
+                    cout << "\033[42m  \033[0m" << " ";
+                } else {
+                    cout << "\033[41m  \033[0m" << " ";
+                }
+            }
+            cout << endl;
         }
-        cout << endl;
+        cin >> y;
     }
-    
     
     return 0;
 }
